965eeea2cefc4d415048f43d91e46821c6bbb2dd
Ali <67905570+intelvert@users.noreply.github.com>
2024-12-10 17:21:12 +0100

    Added edit post in 1h feature, minor improvements

--- social_network/feed/urls.py
+++ social_network/feed/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('post/new', views.create_post, name='create_post' ),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('post/<int:post_id>/delete', views.delete_post, name='delete_post'),
-    path('post/<int:post_id>/edit', views.edit_post, name='edit_post'),
     #path()
 ]
--- social_network/feed/views.py
+++ social_network/feed/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
+from django.contrib import messages
+from core.utils import is_editable
+
 from .models import Post, Like
 from .forms import PostForm
 # Create your views here.
 
 def feed(request):
     posts=Post.objects.all().order_by('-created_at') #get all posts on main page
-    return render(request, 'feed/feed.html', {'posts':posts})
+    publishable_posts= [ { 'post': post, 'is_editable':is_editable(request.user, post, 1)[0]} for post in posts ] #posts ready for publishing in the feed with "editability" on/off
+    return render(request, 'feed/feed.html', {'publishable_posts': publishable_posts})
+
 
 @login_required
 def create_post(request):
@@ -23,9 +28,31 @@ def create_post(request):
         form=PostForm()
     return render(request, 'feed/create_post.html', {'form':form})
 
+
 def post_detail(request, post_id):
     post=get_object_or_404(Post, id=post_id)
-    return render(request, 'feed/post_detail.html', {'post':post})
+    can_edit, error_message = is_editable(request.user, post, 1)
+    
+    form=None
+    if can_edit:
+        if request.method=='POST':
+            form=PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post.been_edited=True
+                form.save()
+                messages.success(request, "Post updated successfully!")
+                return redirect('post_detail', post_id=post.id)
+        elif request.method=='GET':
+            form=PostForm(instance=post)
+    else:
+        messages.error(request, error_message)
+        
+    context={'post': post,
+             'form': form,
+             'is_editable': can_edit,}
+    
+    return render(request, 'feed/post_detail.html', context=context)
+
 
 @login_required
 def delete_post(request, post_id):
@@ -36,20 +63,5 @@ def delete_post(request, post_id):
         post.delete()
         return redirect('feed')
     return render(request, 'feed/confirm_delete.html', {'post':post})
-
-@login_required
-def edit_post(request, post_id):
-    post=get_object_or_404(Post, id=post_id)
-    if post.author!=request.user:
-        return HttpResponseForbidden("You are not allowed to edit this post.")
-    if request.method=='POST':
-        form=PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post.been_edited=True
-            form.save()
-            return redirect('post_detail', post_id=post.id)
-    elif request.method=='GET':
-        form=PostForm(instance=post)
-    return render(request, 'feed/edit_post.html', {'form':form, 'post':post})
 
 #def like_post
